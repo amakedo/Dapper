774337219ed4dc9d86a14c7e0eedec19f59cc9f2
Amakedo <korole793@gmail.com>
2025-11-10 18:38:45 +0200

    Add project files.

---
+++ Dapper/Program.cs
@@ -0,0 +1,24 @@
+﻿using Dapper_1;
+using System.Data.SqlClient;
+
+namespace Dapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string connStr = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Students;Integrated Security=True;Connect Timeout=30;";
+
+            var db = new SqlConnection(connStr);
+
+            string sql = "INSERT INTO Students (Name, Age, Adress, Email, Year) VALUES (@Name, @Age, @Adress, @Email, @Year)";
+
+            var student = new Student {Name = "John Doe", Age = 20, Adress = "123 Main St", Email = "test@gmail.com", Year = 2 };
+
+            int rows = db.Execute(sql, student);
+            Console.WriteLine($"{rows} row(s) inserted");
+        }
+    }
+}
---
+++ Dapper/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace Dapper_1
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Adress { get; set; }
+
+        public string Email { get; set; }
+
+        public int Year { get; set; }
+    }
+}
+
